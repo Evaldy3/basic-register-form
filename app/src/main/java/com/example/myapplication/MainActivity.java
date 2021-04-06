e577a61483f629f7b2ea4602ee4cb1455bf95918
Evaldy3 <evaldyaristil@gmail.com>
2021-04-05 22:11:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void RegisterClick(View view) {
+        TextView txt1 = findViewById(R.id.txt1);
+        TextView txt2 = findViewById(R.id.txt2);
+        TextView txt3 = findViewById(R.id.txt3);
+        EditText editFirstName = findViewById(R.id.editFirstName);
+        EditText editLastName = findViewById(R.id.editLastName);
+        EditText editEmail = findViewById(R.id.editEmail);
+
+        txt1.setText("FirstName:" +  editFirstName.getText().toString());
+        txt2.setText("LastName:" +  editLastName.getText().toString());
+        txt3.setText("Email:" +  editEmail.getText().toString());
+    }
+}
